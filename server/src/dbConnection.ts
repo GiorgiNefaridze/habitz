b423a0575f3761252b92abfd77ecc12c85f8bbcb
Giorgi Nefaridze <nefogio1@gmail.com>
2023-10-11 23:32:05 +0400

    make connection to postgre db

---
+++ server/src/dbConnection.ts
@@ -0,0 +1,12 @@
+import { Pool } from "pg";
+import { config } from "dotenv";
+
+config();
+
+export const connection = new Pool({
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  port: Number(process.env.PORT),
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+});
